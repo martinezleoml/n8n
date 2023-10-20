yksirotta/n8nnodepython

refactor(core): Refactor node-details endpoints (no-changelog)

packages/cli/src/controllers/index.ts
@@ -2,6 +2,7 @@ export { AuthController } from './auth.controller';
 export { LdapController } from './ldap.controller';
 export { MeController } from './me.controller';
 export { MFAController } from './mfa.controller';
+export { NodeDetailsController } from './nodeDetails.controller';
 export { NodeTypesController } from './nodeTypes.controller';
 export { OwnerController } from './owner.controller';
 export { PasswordResetController } from './passwordReset.controller';

martinezleoml/n8n

packages/cli/src/controllers/index.ts
@@ -1,4 +1,5 @@
 export { AuthController } from './auth.controller';
+export { DynamicNodeParametersController } from './dynamicNodeParameters.controller';
 export { LdapController } from './ldap.controller';
 export { MeController } from './me.controller';
 export { MFAController } from './mfa.controller';